da957c17a0d779ab6e1b89f6eb9235dc7c4e5600
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2015-08-06 23:34:41 +0200

    Add docs to Account class

--- src/Gravatar/Account.php
+++ src/Gravatar/Account.php
@@ -6,26 +6,57 @@
 
 namespace Gravatar;
 
+/**
+ * Gravatar account
+ *
+ * @author Krzysztof Piasecki <krzysiekpiasecki@gmial.com>
+ */
 class Account
 {
-    
+
+    /**
+     * Account email
+     *
+     * @var string Account email
+     */
     protected $email = "";
-    
+
+    /**
+     * New Gravatar account
+     *
+     * @param $email Account email
+     */
     public function __construct($email)
     {
         $this->email = $email;
     }
-    
-    public function getEmail() 
+
+    /**
+     * Get account email
+     *
+     * @return string Account email
+     */
+    public function getEmail()
     {
         return $this->email;
     }
-    
+
+    /**
+     * Get account email
+     *
+     * @return string Account email
+     */
     public function __toString()
     {
         return $this->email;
     }
 
+    /**
+     * Ensure that two Accounts are equal
+     *
+     * @param Account $account Other account
+     * @return bool if equal return true otherwise false
+     */
     public function equals(Account $account)
     {
         return $this == $account;
